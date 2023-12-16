ee8cdd898069f57ada14411788110830c388bde6
syahrul jay <syahruljaylani73@gmail.com>
2023-12-17 01:47:30 +0700

    change second at type writter

--- src/Components/TypeWritter/index.jsx
+++ src/Components/TypeWritter/index.jsx
@@ -8,7 +8,7 @@ const TypeWritter = () => {
       <motion.div className="flex justify-center text-white h-screen items-center" initial={{ opacity: 0, x: 0 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0 }} transition={{ duration: 1 }}>
         <Typewriter
           onInit={(typewriter) => {
-            typewriter.typeString('Teknik informatika').pauseFor(300).deleteAll().typeString('Universitas Wijaya Kusuma').pauseFor(400).deleteAll().start();
+            typewriter.typeString('Teknik informatika').pauseFor(300).deleteAll().typeString('Universitas Wijaya Kusuma').pauseFor(400).start();
           }}
         />
       </motion.div>
